d5da7ca68da32eb05cd28845d9aceaa9a72594b9
aaitbenelarbi <153749740+aaitbenelarbi@users.noreply.github.com>
2023-12-19 23:47:47 +0100

    Add files via upload

---
+++ shared_data.js
@@ -0,0 +1,21 @@
+const shared_data = {
+    footer: {
+        privacyPolicy: "https://secureidentity.schneider-electric.com/identity/VFP_IDMSPrivacySelector?app=mySchneiderRegV2&code_challenge=dscLeQNAPr0ak0lSnIl699PXI15Atc2G3n9cQ-rjkQ0&code_challenge_method=S256&countryCode=FR&lang=en&redirect_uri=https%3A%2F%2Fwww.se.com%2Fmyschneider%2Fauth%2Fsignin-callback&response_type=code&state=8b320c5292984fea9983f86fedd82ebe",
+        privacyNotice: "https://secureidentity.schneider-electric.com/identity/VFP_IDMSPrivacySelector?app=mySchneiderWeb&lang=en&privacyNotice=showPrivacyNotice",
+        needHelpUrl: "http://localhost:4200/need-help"
+    },
+    header: {
+        icon: "https://se.file.force.com/servlet/servlet.ImageServer?id=0158V00000Doiff&oid=00DA0000000abSm",
+    },
+    cookieConsent: {
+        urls: {
+            token: "",
+            consent: "",
+        },
+        keys: {
+            a: "",
+            b: "",
+            c: ""
+        }
+    }
+}
