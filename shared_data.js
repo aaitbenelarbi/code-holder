a8e877e330148f50ccc85678724a38d8b827b9b7
aaitbenelarbi <153749740+aaitbenelarbi@users.noreply.github.com>
2023-12-20 01:00:11 +0100

    Update shared_data.js

--- shared_data.js
+++ shared_data.js
@@ -1,4 +1,4 @@
-const shared_data = {
+window.shared_data = {
     footer: {
         privacyPolicy: "https://secureidentity.schneider-electric.com/identity/VFP_IDMSPrivacySelector?app=mySchneiderRegV2&code_challenge=dscLeQNAPr0ak0lSnIl699PXI15Atc2G3n9cQ-rjkQ0&code_challenge_method=S256&countryCode=FR&lang=en&redirect_uri=https%3A%2F%2Fwww.se.com%2Fmyschneider%2Fauth%2Fsignin-callback&response_type=code&state=8b320c5292984fea9983f86fedd82ebe",
         privacyNotice: "https://secureidentity.schneider-electric.com/identity/VFP_IDMSPrivacySelector?app=mySchneiderWeb&lang=en&privacyNotice=showPrivacyNotice",
@@ -20,9 +20,9 @@ const shared_data = {
     }
 }
 
-const need_help = {
+window.need_help = {
     support: {
-        url : "https://se.com/",
+        url: "https://se.com/",
         email: "aaitbenelarbi@deloitte.fr"
     }
 }
